39b2e5842ff69721c56d6632e11080be36a1f8a9
sebasrival <sebasrival@gmail.com>
2023-02-10 04:49:24 +0000

    fix: change background color

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,11 @@
-import { CategoryNapoliList } from "./CategoryNapoliList"
 import { TableNapoliResults } from "./TableNapoliResults"
 
 
 export const Dashboard = () => {
   return (
-    <div className='flex h-screen w-full bg-slate-800 text-white'>
+    <div className='flex h-full w-full bg-slate-800 text-white'>
       <div className="flex flex-col mx-auto lg:w-9/12 xl:w-7/12 w-full">
-        <div className="flex flex-col pt-10 bg-slate-800">
+        <div className="flex flex-col pt-10 ">
           <div className="flex flex-row justify-center items-center">
             <h1 className="text-3xl font-bold">Napoli Results</h1>
           </div>
